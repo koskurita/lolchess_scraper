5a77e9eb7303adbea3db52d4125ff29d9bf7b0b9
koskurita <kosuke.kurita@gmail.com>
2023-07-22 15:04:29 -0700

    scrape larger dataset and concurrency

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 from bs4 import BeautifulSoup
+import concurrent.futures
 import requests
 import csv
 import ast
 import time
+from datetime import datetime
 
-# start_time = time.time()
+NUM_THREADS = 20
+last_patch = "07-18-2023"
+patch_date = datetime.strptime(last_patch, "%m-%d-%Y")
 
 def scrape(summoner_name):
-    # ts = start_time -time.time()
-    # print("started scraping", summoner_name, ": ", ts)
     all_data = []
     _url = 'https://lolchess.gg/profile/na/{}/s9/matches/ranked'.format(summoner_name)
-    _page_number =[ '', '/2']
-    for i in range(2):
+    _page_number =[ '', '/2', '/3']
+    for i in range(3):
         r = requests.get(_url+_page_number[i])
         source = r.content
         soup = BeautifulSoup(r.content, 'lxml')
         games = soup.find_all('div', {"class": "profile__match-history-v2__item"})
         for game in games:
+            _date = game.find('div', {"class": "age"})
+            # dates have 10 digits
+            game_date = game.find('span').get('title')[:10]
+            newdate = datetime.strptime(game_date, "%m-%d-%Y")
+            if newdate < patch_date:
+                break;
+            
             match_data = []
             
             # class has last char as the placement
@@ -28,73 +37,72 @@ def scrape(summoner_name):
             for aug in augs:
                 _augments.append(aug.find('img').attrs['alt'])
             
-            _champions = []
-            units = game.find_all('div', {"class": "unit"})
-            for unit in units:
-                x = unit.find('img', {"class": "stars"})
-                # special case for apex turret
-                if x:
-                    # stars is the [-5] char in src
-                    _star = x.get('src')[-5]
-                else:
-                    _star = 0
-                y = unit.find ('div', {"class": "tft-champion"})
-                _champion = (y.find('img').attrs['alt'])
-                _champions.append((_champion, _star))
+            # _champions = []
+            # units = game.find_all('div', {"class": "unit"})
+            # for unit in units:
+            #     x = unit.find('img', {"class": "stars"})
+            #     # special case for apex turret
+            #     if x:
+            #         # stars is the [-5] char in src
+            #         _star = x.get('src')[-5]
+            #     else:
+            #         _star = 0
+            #     y = unit.find ('div', {"class": "tft-champion"})
+            #     _champion = (y.find('img').attrs['alt'])
+            #     _champions.append((_champion, _star))
                 
-            _traits = []
-            traits = game.find_all('div', {"class": "tft-hexagon-image"})
-            for trait in traits:
-                # lolchess format ex. trait = "1 Darkin"
-                t_cnt = trait.find('img').attrs['alt'][0]
-                t = trait.find('img').attrs['alt'][2:]
-                _traits.append((t, t_cnt))
+            # _traits = []
+            # traits = game.find_all('div', {"class": "tft-hexagon-image"})
+            # for trait in traits:
+            #     # lolchess format ex. trait = "1 Darkin"
+            #     t_cnt = trait.find('img').attrs['alt'][0]
+            #     t = trait.find('img').attrs['alt'][2:]
+            #     _traits.append((t, t_cnt))
             
-            match_data = [_placement, _augments, _traits, _champions]
+            # match_data = [_placement, _augments, _traits, _champions]
+            
+            match_data = [_placement, _augments]
             all_data.append(match_data)
             
     filename = '{}.csv'.format(summoner_name)
-    # ts = start_time -time.time()
-    # print("finished scraping", summoner_name, ": ", ts)
     write_csv_file(filename, all_data)
     return 
     
     
 def write_csv_file(filename, data):
-    # ts = start_time -time.time()
-    # print("started writing in file: ", ts)
     with open (filename, 'w') as new_file:
-        fieldnames = ['placement', 'augments', 'traits', 'champions']
+        # fieldnames = ['placement', 'augments', 'traits', 'champions']
+        fieldnames = ['placement', 'augments']
         csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
         csv_writer.writeheader()
-        for pl, au, tr, ch in data:
-            csv_writer.writerow({'placement': pl, 'augments': au, 'traits': tr, 'champions': ch})
-    # ts = start_time -time.time()
-    # print("finished writing in file: ", ts)
+        for pl, au in data:
+            # csv_writer.writerow({'placement': pl, 'augments': au, 'traits': tr, 'champions': ch})
+            csv_writer.writerow({'placement': pl, 'augments': au})
     return 
             
 def find_top_players():        
-    # ts = start_time -time.time()
-    # print("finding top players: ", ts)
     player_list = []
-
-    top_players = requests.get('https://lolchess.gg/leaderboards?mode=ranked&region=na')
-    soup = BeautifulSoup(top_players.content, 'lxml')
-    games = soup.find_all('td', {"class": "summoner"})
-    for game in games:
-        player = game.find('a')
-        summonername = player.get_text()
-        # summoner name has white spaces which produces error when creating file
-        player_list.append(summonername.strip())
-    # ts = start_time -time.time()
-    # print("finsihed finding top players: ", ts)
+    _page_number =[ '', '&page=2', '&page=3']
+    for i in range(3):
+        top_players = requests.get('https://lolchess.gg/leaderboards?mode=ranked&region=na{}'.format(_page_number[i]))
+        soup = BeautifulSoup(top_players.content, 'lxml')
+        games = soup.find_all('td', {"class": "summoner"})
+        for game in games:
+            player = game.find('a')
+            summonername = player.get_text()
+            # summoner name has white spaces which produces error when creating file
+            player_list.append(summonername.strip())
     return player_list
 
 def scrape_all(player_list):
-    # ts = start_time -time.time()
-    # print("started scraping: ", ts)
-    for player in player_list:
-        scrape(player)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+        executor.map(scrape, player_list)
+    
+    # for player in player_list:
+    #     scrape(player)
+
+
 
 
     
@@ -145,7 +153,8 @@ def augment_stat():
 # find the list of csv files using a list of player names and combine it into combined.csv
 def combine_files(player_list):
     with open("combined.csv", 'w') as new_file:
-        fieldnames = ['placement', 'augments', 'traits', 'champions']
+        # fieldnames = ['placement', 'augments', 'traits', 'champions']
+        fieldnames = ['placement', 'augments']
         csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
         csv_writer.writeheader()
         for p in player_list:
@@ -172,8 +181,10 @@ def run():
     print_stat(30)
 
 
+start_time = time.time()
+print("Start", start_time-start_time)
 run()
-
-
+ts = time.time() -start_time
+print("Finish", ts)
 
     
